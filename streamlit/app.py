667b5fbfce3c728d7ca5255ec7ba81d2e32235f0
amir mohammad arghavany <143076005+999-amir@users.noreply.github.com>
2024-03-06 22:23:31 +0330

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -3,13 +3,13 @@ import joblib
 from utils import PreProcessor
 import streamlit as st
 
-model = joblib.load('HeartAttack.joblib')
-def predict(data):
-    data_features = ['ID', 'Name', 'Age', 'Gender', 'Height(cm)', 'Weight(kg)',
-        'Blood Pressure(mmHg)', 'Cholesterol(mg/dL)', 'Glucose(mg/dL)',
-        'Smoker', 'Exercise(hours/week)']
-    data_df = pd.DataFrame([['n', 'n']+data], columns=data_features)
-    return model.predict(data_df)[0]
+# model = joblib.load('HeartAttack.joblib')
+# def predict(data):
+#     data_features = ['ID', 'Name', 'Age', 'Gender', 'Height(cm)', 'Weight(kg)',
+#         'Blood Pressure(mmHg)', 'Cholesterol(mg/dL)', 'Glucose(mg/dL)',
+#         'Smoker', 'Exercise(hours/week)']
+#     data_df = pd.DataFrame([['n', 'n']+data], columns=data_features)
+#     return model.predict(data_df)[0]
 # data = [23, 'Male', 173, 56, '120/80', 170, 80, 'No', 2]
 # predict(data)
 
